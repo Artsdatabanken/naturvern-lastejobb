4b3e4add134f9557a42bb8dfb89a70eb23287617
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-19 12:34:31 +0200

    Cleaner code

--- script/01_last_ned/wikidata/kjør_sparql_mot_wikidata.js
+++ script/01_last_ned/wikidata/kjør_sparql_mot_wikidata.js
@@ -3,17 +3,11 @@ const log = require("log-less-fancy")();
 const { io } = require("lastejobb");
 const path = require("path");
 
-const queries = io.findFiles("./script/01_last_ned/wikidata", ".sparql");
-function next() {
-  const query = queries.pop();
-  if (!query) return;
-  log.info("Sparql: " + query);
-  wikidata
-    .queryFromFile(query, path.basename(query))
-    .then(r => next())
-    .catch(err => {
-      log.error(err.message);
-      process.exit(1);
-    });
-}
-next();
+const query = "./script/01_last_ned/wikidata/naturvernområde.sparql";
+wikidata
+  .queryFromFile(query, path.basename(query))
+  .then(r => next())
+  .catch(err => {
+    log.error(err.message);
+    process.exit(1);
+  });
