a9f9398f2bb22697dcf0425226080907604fe11d
Bjørn Reppen <bjornreppen@users.noreply.github.com>
2019-04-22 18:18:59 +0200

    Move sparql queries to root

--- script/01_last_ned/kjør_sparql_mot_wikidata.js
+++ script/01_last_ned/kjør_sparql_mot_wikidata.js
@@ -1,9 +1,9 @@
-const wikidata = require("../../../wikidata");
+const wikidata = require("../../wikidata");
 const log = require("log-less-fancy")();
 const { io } = require("lastejobb");
 const path = require("path");
 
-const query = "./script/01_last_ned/wikidata/wikidata_naturvernområde.sparql";
+const query = "./wikidata/naturvernområde.sparql";
 wikidata.queryFromFile(query, path.basename(query)).catch(err => {
   log.error(err.message);
   process.exit(1);
