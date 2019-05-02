737c680638b08e7d4774b25e502b571363f5aecb
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-02 11:38:42 +0200

    Forvaltningsinstanser (i fylker)

---
+++ script/02_map/forvaltningsinstans.js
@@ -0,0 +1,17 @@
+const { io } = require("lastejobb");
+
+let fylker = io.lesDatafil("fylke.json").items;
+const r = lagFylkesmann(fylker);
+io.skrivBuildfil(__filename, { items: r });
+
+function lagFylkesmann(kilde) {
+  const r = [];
+  kilde.forEach(o => {
+    const e = {
+      navn: { nor: "Fylkesmannen i " + o.itemLabel },
+      kode: "VV-FM-FM-" + o.code.replace("NO-", "")
+    };
+    r.push(e);
+  });
+  return r;
+}
