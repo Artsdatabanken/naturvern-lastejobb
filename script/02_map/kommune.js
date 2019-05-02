21452c87b3027f6e8760bca0f66e1c2e27e09b03
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-02 11:34:19 +0200

    Naturvern i kommuner og fylker

---
+++ script/02_map/kommune.js
@@ -0,0 +1,20 @@
+const { io } = require("lastejobb");
+
+let fylker = io.lesDatafil("fylke.json").items;
+let kommuner = io.lesDatafil("kommune.json").items;
+
+const r = [];
+lagKoder(r, fylker, "fylke");
+lagKoder(r, kommuner, "kommune");
+io.skrivBuildfil(__filename, r);
+
+function lagKoder(r, kilde, nivå) {
+  kilde.forEach(o => {
+    const key = "VV-AO-" + o.code.replace("NO-", "");
+    const e = {
+      kode: key,
+      navn: { nor: "Naturvernområde i " + o.itemLabel + " " + nivå }
+    };
+    r.push(e);
+  });
+}
