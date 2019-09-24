3e17dd2100b00dd36a66c9fa2773953b51f5067b
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-09-24 20:12:54 +0200

    Lag typer for verneform

--- stages/transform/80/type.js
+++ stages/transform/80/type.js
@@ -2,17 +2,11 @@ const { io } = require("lastejobb");
 
 const r = [];
 include("data/relasjon.json");
+include("data/naturvern-ubehandlet/verneform.json");
 
 function include(fn) {
   let rot = io.readJson(fn);
-  rot.items.forEach(e => {
-    r.push(e);
-    if (e.definisjon) include("./data/naturvern-ubehandlet/" + e.definisjon);
-  });
+  rot.items.forEach(e => r.push(e));
 }
 
-/*const forvaltningsinstans = io.lesBuildfil("forvaltningsinstans").items;
-forvaltningsinstans.forEach(fi => r.push(fi));
-const kommune = io.lesBuildfil("kommune").items;
-kommune.forEach(ko => r.push(ko));*/
 io.skrivBuildfil(__filename, r);
