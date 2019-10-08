19baf802137d55dd3f1a79d6bc086b6e7b880391
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-08 11:07:12 +0200

    Fix: Manglende tittel på kategorier

--- stages/transform/90/type.js
+++ stages/transform/90/type.js
@@ -3,7 +3,9 @@ const { io, url, json } = require("lastejobb");
 const tre = io.lesDatafil("alle");
 new url(tre).assignUrls();
 Object.values(tre).forEach(e => {
-  e.tittel.nob = e.offisieltNavn;
-  delete e.offisieltNavn;
+  if (e.offisieltNavn) {
+    e.tittel.nob = e.offisieltNavn;
+    delete e.offisieltNavn;
+  }
 });
 io.skrivBuildfil(__filename, tre);
