9af14f777dae046874841bdf36d71202125ba320
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-08 12:57:14 +0200

    Fjern informasjon som ligger i relasjoner

--- stages/transform/90/type.js
+++ stages/transform/90/type.js
@@ -6,6 +6,9 @@ Object.values(tre).forEach(e => {
   if (e.offisieltNavn) {
     e.tittel.nob = e.offisieltNavn;
     delete e.offisieltNavn;
+    delete e.verneform;
+    delete e.verneplan;
+    delete e.vurdering;
   }
 });
 io.skrivBuildfil(__filename, tre);
