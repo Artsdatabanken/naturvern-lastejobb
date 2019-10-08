5b16a3708c51fbd3770947f215801e44a818dec4
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-08 13:31:32 +0200

    Fjern redundante nøkler

--- stages/transform/90/type.js
+++ stages/transform/90/type.js
@@ -6,12 +6,12 @@ Object.values(tre).forEach(e => {
   if (e.offisieltNavn) {
     e.tittel.nob = e.offisieltNavn;
     delete e.offisieltNavn;
-
-    // Ligger som relasjoner
-    delete e.verneform;
-    delete e.verneplan;
-    delete e.vurdering;
-    delete e.forvaltning;
   }
+
+  // Ligger som relasjoner
+  delete e.verneform;
+  delete e.verneplan;
+  delete e.vurdering;
+  delete e.forvaltning;
 });
 io.skrivBuildfil(__filename, tre);
