a7a87a101630479642fd1ba8d5196793a6599240
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-12-08 15:35:44 +0100

    Dataflyt

--- stages/download/ubehandlet.js
+++
@@ -1,6 +0,0 @@
-const { git } = require("lastejobb");
-
-git.clone(
-  "https://github.com/Artsdatabanken/naturvern-ubehandlet.git",
-  "temp/naturvern-ubehandlet"
-);
--- stages/transform/20/logo.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const { io } = require("lastejobb");
-
-io.mkdir("build/logo");
-fs.copyFileSync(
-  "temp/naturvern-ubehandlet/logo/logo.svg",
-  "build/logo/logo.svg"
-);
